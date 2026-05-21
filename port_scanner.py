3013b187f3be825d78840094a648e4186759f77f
Lovish123-tech <t01l25@abdn.ac.uk>
2026-05-21 10:06:48 +0100

    Initial commit - port scanner project

---
+++ port_scanner.py
@@ -0,0 +1,35 @@
+import socket
+
+services = { 
+    80: 'HTTP',
+    442: 'HTTPS',
+    22: 'SHS',
+    21: 'FTP'
+    }
+
+target = ('google.com')
+ports = [80, 442, 22, 21]
+
+open_port = 0
+close_port = 0
+
+print(f"\nScanning Target\n")
+
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} Is Open {services[port]}")
+        open_port += 1
+    else:
+        print(f"Port {port} Is Close")
+        close_port += 1
+
+s.close()
+print("------------------SCAN COMPLETED------------------")
+print("OPEN PORTS: ", {open_port})
+print("CLOSE PORTS: ", {close_port})
