b6740439c2274388b4b58cdb18c2648c72b25fc7
Lovish123-tech <t01l25@abdn.ac.uk>
2026-05-21 10:17:18 +0100

    Fix bugs in port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -1,14 +1,14 @@
 import socket
 
-services = { 
+services = {
     80: 'HTTP',
-    442: 'HTTPS',
-    22: 'SHS',
+    443: 'HTTPS',
+    22: 'SSH',
     21: 'FTP'
-    }
+}
 
-target = ('google.com')
-ports = [80, 442, 22, 21]
+target = 'google.com'
+ports = [80, 443, 22, 21]
 
 open_port = 0
 close_port = 0
@@ -17,19 +17,18 @@ print(f"\nScanning Target\n")
 
 for port in ports:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     s.settimeout(1)
-
     result = s.connect_ex((target, port))
 
     if result == 0:
-        print(f"Port {port} Is Open {services[port]}")
+        print(f"Port {port} Is Open - {services[port]}")
         open_port += 1
     else:
-        print(f"Port {port} Is Close")
+        print(f"Port {port} Is Closed")
         close_port += 1
 
-s.close()
+    s.close()
+
 print("------------------SCAN COMPLETED------------------")
-print("OPEN PORTS: ", {open_port})
-print("CLOSE PORTS: ", {close_port})
+print("OPEN PORTS: ", open_port)
+print("CLOSE PORTS: ", close_port)
